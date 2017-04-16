58845bb49e52713503d155e841d6bb3babd12da3
thechutrain <chu.chuyalan@gmail.com>
2017-04-15 21:19:49 -0400

    working register page

--- server/controllers/authRouter.js
+++ server/controllers/authRouter.js
@@ -4,9 +4,50 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
 router.post('/register', (req, res) => {
-  console.log('POST request to /auth/register')
-  // TO DO save user to the database
-  res.json({ request: req.body })
+  let {username, password, password2} = req.body.data
+  // console.log(username)
+  // console.log(password)
+  // console.log(password2)
+  // console.log('=============')
+  const errorsArray = []
+  let isValid = true
+  if (password !== password2) {
+    errorsArray.push('Passwords must match')
+    isValid = false
+  } else if (password === "") {
+    errorsArray.push('Password cannot be empty')
+    isValid = false
+  }
+  // TO DO more validation??
+  if (!isValid) {
+    return res.json({ error: true, errorsArray })
+  }
+
+  // check if there is not a user names whatever
+  User.find({ username }, (err, match) => {
+    if (err) {
+      return res.json({ error: true, errorsArray: ['Error in finding User']})
+    } else if (match.length !==0) {
+      return res.json({ error: true, errorsArray: ['Already a user with that username']})
+    }
+
+    const userData = new User({
+      username,
+      password,
+    })
+    // console.log(userData) // new object + _id
+
+    userData.save((err) => {
+        console.log('saving ...')
+        if (err) {
+          console.log('saving error!')
+          return res.json({ error: true, errorsArray: ['Saving Error']})
+        }
+        console.log('SUCCES!!')
+        return res.json({ success: true })
+    })
+    // return res.json({ error: true, errorsArray: ['Mongoose Error'] })
+  })
 })
 
 router.post('/login', (req, res) => {
--- server/public/main.js
+++ server/public/main.js
@@ -1,79 +1,79 @@
-$(document).ready(function() {
-  console.info('page was loaded')
-  // =========  TESTING Event Listeners ==============
-  $('a#private-link').on('click', function(event) {
-    event.preventDefault()
-    // console.log('a#private-link was pressed yooo')
-    var token = localStorage.getItem('token') || null
-    axios.get('/private', {
-      headers: { token: token }
-    }).then(function(response) {
-      // console.log(response)
-      if (response.data.error) {
-        console.log('error')
-        var msg = $('<p>').text(JSON.stringify(response.data.msg))
-        $('div#data-container').append(msg)
-      } else {
-        var clean_data = JSON.stringify(response.data, null, 4)
-        $('div#data-container').append(clean_data)
-      }
-    })
-  })
+// $(document).ready(function() {
+//   console.info('page was loaded')
+//   // =========  TESTING Event Listeners ==============
+//   $('a#private-link').on('click', function(event) {
+//     event.preventDefault()
+//     // console.log('a#private-link was pressed yooo')
+//     var token = localStorage.getItem('token') || null
+//     axios.get('/private', {
+//       headers: { token: token }
+//     }).then(function(response) {
+//       // console.log(response)
+//       if (response.data.error) {
+//         console.log('error')
+//         var msg = $('<p>').text(JSON.stringify(response.data.msg))
+//         $('div#data-container').append(msg)
+//       } else {
+//         var clean_data = JSON.stringify(response.data, null, 4)
+//         $('div#data-container').append(clean_data)
+//       }
+//     })
+//   })
 
-  $('a#logout-link').on('click', function(event) {
-    event.preventDefault()
-    console.log(localStorage.getItem('token'))
-    localStorage.removeItem('token')
-    console.log('cleared token ...')
-    console.log(localStorage.getItem('token'))
-  })
-  // ========= I. Event Listeners ==============
-  $('button#login-btn').on('click', function(event) {
-    event.preventDefault()
-    var formData = {}
-    $('form > div.form-group').each(function(i, element){
-      var key = $(element).find('label').attr('for').trim()
-      var value = $(element).find('input').val().trim()
-      formData[key] = value
-    })
-    // console.dir(formData)
+//   $('a#logout-link').on('click', function(event) {
+//     event.preventDefault()
+//     console.log(localStorage.getItem('token'))
+//     localStorage.removeItem('token')
+//     console.log('cleared token ...')
+//     console.log(localStorage.getItem('token'))
+//   })
+//   // ========= I. Event Listeners ==============
+//   $('button#login-btn').on('click', function(event) {
+//     event.preventDefault()
+//     var formData = {}
+//     $('form > div.form-group').each(function(i, element){
+//       var key = $(element).find('label').attr('for').trim()
+//       var value = $(element).find('input').val().trim()
+//       formData[key] = value
+//     })
+//     // console.dir(formData)
 
-    // 2. make ajax request
-    $.ajax({
-      url: '/auth/login',
-      data: formData,
-      method: 'POST'
-    })
-    .then(function(response) {
-      console.log(response)
-      if (!response.error) {
-        console.log('setting the local storage w token')
-        localStorage.setItem('token', response.token)
-      }
-    })
-  }) // ends login-btn event listener
+//     // 2. make ajax request
+//     $.ajax({
+//       url: '/auth/login',
+//       data: formData,
+//       method: 'POST'
+//     })
+//     .then(function(response) {
+//       console.log(response)
+//       if (!response.error) {
+//         console.log('setting the local storage w token')
+//         localStorage.setItem('token', response.token)
+//       }
+//     })
+//   }) // ends login-btn event listener
 
-  // ========= II. Event Listeners ==============
-  $('button#register-btn').on('click', function(event) {
-    event.preventDefault()
-    // 1. Get the form data
-    var formData = {}
-    $('form > div.form-group').each(function(i, element){
-      var key = $(element).find('label').attr('for').trim()
-      var value = $(element).find('input').val().trim()
-      formData[key] = value
-    })
-    console.dir(formData)
+//   // ========= II. Event Listeners ==============
+//   $('button#register-btn').on('click', function(event) {
+//     event.preventDefault()
+//     // 1. Get the form data
+//     var formData = {}
+//     $('form > div.form-group').each(function(i, element){
+//       var key = $(element).find('label').attr('for').trim()
+//       var value = $(element).find('input').val().trim()
+//       formData[key] = value
+//     })
+//     console.dir(formData)
 
-    // 2. make ajax request
-    $.ajax({
-      url: '/auth/register',
-      data: formData,
-      method: 'POST'
-    })
-    .then(function(response) {
-      console.log(response)
-    })
-  }) // ends register-btn event listener
+//     // 2. make ajax request
+//     $.ajax({
+//       url: '/auth/register',
+//       data: formData,
+//       method: 'POST'
+//     })
+//     .then(function(response) {
+//       console.log(response)
+//     })
+//   }) // ends register-btn event listener
 
-})
+// })
