6b82260fd53d6bc32fd6abcefa1b247b45e75dbe
thechutrain <chu.chuyalan@gmail.com>
2017-04-18 11:18:42 -0400

    added cookie parser

--- server/server.js
+++ server/server.js
@@ -1,17 +1,30 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
 const path = require('path')
+const parseTokenCookie = require('./middleware/checkAuth').parseTokenCookie
 require('dotenv').load()
 
 // ========== Create express app ============
 const app = express()
-const PORT = process.env.PORT || 3000
 app.set('x-powered-by', false)
+const PORT = process.env.PORT || 3000
 
 // ========== middleware ============
 app.use(morgan('dev'))
+app.use(cookieParser())
+app.use(parseTokenCookie()) // custom middleware function I wrote to parse the cookie
+//  ======= testing, cookie and req ==========
+// app.use((req, res, next) => {
+//   console.log('=========COOOKIES=======')
+//   console.log(req.cookies)
+//   console.log('========= END of COOOKIES=======')
+//   next()
+// })
+
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')))
