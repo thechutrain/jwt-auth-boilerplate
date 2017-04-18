8b93a2931887f8320d0120daabb9215b7e7f739b
thechutrain <chu.chuyalan@gmail.com>
2017-04-18 11:19:15 -0400

    added more text to msg

--- server/controllers/apiRouter.js
+++ server/controllers/apiRouter.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const adminOnly = require('../middleware/checkAuth').adminOnly
-const authUserOnly = require('../middleware/checkAuth').authUserOnly
+const anyUserOnly = require('../middleware/checkAuth').anyUserOnly
 // const jwt = require('jsonwebtoken')
 
-router.get('/logged-in-only/data', authUserOnly(), (req, res) => {
-  const user = req.user
-  let msg = 'Super secret message for this user ...' + JSON.stringify(user, null, 4)
+router.get('/logged-in-only/data', anyUserOnly(), (req, res) => {
+  const user = JSON.stringify(req.user, null, 4)
+  let msg = `
+  ====== Logged in only Data =======
+  This is a message for logged in only users ...
+  Here is your token info:
+  ${user}
+  `
   res.json({ msg })
 })
 
 router.get('/admin-only/data', adminOnly(), (req, res) => {
-  // const user = req.user
-  // console.log(user)
+
   const msg = 'Super secret message for this ADMIN ONLY!! ;)'
   res.json({ msg })
 })
