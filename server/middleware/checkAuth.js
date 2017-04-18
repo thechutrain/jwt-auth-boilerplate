f9d58a9b28ff2f3b0106352ae268f23cbc3aefbf
thechutrain <chu.chuyalan@gmail.com>
2017-04-18 11:21:26 -0400

    refactored and simplified check auth middleware

--- server/middleware/checkAuth.js
+++ server/middleware/checkAuth.js
@@ -3,30 +3,54 @@ const jwt = require('jsonwebtoken')
 
 module.exports.adminOnly = () => {
   return function (req, res, next) {
-    const token = req.body.token || req.headers['token']
-    console.log('Token: ', token)
-    jwt.verify(token, process.env.JWT_PASSPHRASE, (err, decoded) => {
-      if (err) {
-        return res.json({error: true, msg: 'Invalid Token'})
-      }
-      // TO DO? check if the user exists?
-      if (!decoded.isAdmin) {
-        return res.json({error: true, msg: 'Admin only access'})
-      }
-      console.log(decoded)
-      req.user = decoded
-      next()
-    }) // ends jwt.verity
+    if (req.user === null) {
+      return res.status(401).send('Admin only')
+    } else if (!req.user.isAdmin) {
+      return res.status(401).send('Admin only')
+    }
+    next()
   }
 }
 
-module.exports.authUserOnly = () => {
+module.exports.anyUserOnly = () => {
   return (req, res, next) => {
-    const token = req.body.token || req.headers['token']
-    jwt.verify(token, process.env.JWT_PASSPHRASE, (err, decoded) => {
-      if (err) return res.json({error: true, msg: 'Invalid Token'})
-      req.user = decoded
+    if (req.user === null) {
+      return res.status(401).send('Logged in Users only')
+    } else if (!req.user) {
+      return res.status(401).send('Logged in Users only')
+    }
+    next()
+  }
+}
+
+module.exports.parseTokenCookie = () => {
+  return (req, res, next) => {
+    // 1. get the token from request cookie
+    const token = req.cookies.token || req.headers['token']
+    // 2. check to see if there is a token ...
+    if (!token) {
+      req.user = null
       next()
-    })
+    } else {
+      // 3. verify the token has not been tampered with ...
+      jwt.verify(token, process.env.JWT_PASSPHRASE, (err, decoded) => {
+        if (err) {
+          req.user = null
+          next()
+        }
+        // 4. valid token save the decoded user data in the request object
+        // so all downstream middleware function have access to user credentials
+        /*
+        ex. of schema of what req.user will look like
+        req.user:  { _id: '58f56dcb6200e636e825383c',
+        username: 'alan',
+        isAdmin: true,
+        exp: 1492526549,
+        iat: 1492522949 }
+        */
+        req.user = decoded
+        next()
+      }) // ends jwt.verity
+    }
   }
 }
