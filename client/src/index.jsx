5532e12b5250850a5493b27f649c0a06b64e0e7a
thechutrain <chu.chuyalan@gmail.com>
2017-04-21 18:51:16 -0400

    simple react app going ...

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Auth from './components/Auth'
 
-const testElement = (
-  <div>
-    <h1>hello world from a react element!</h1>
-  </div>
-)
+// const testElement = (
+//   <div>
+//     <h1>hello world from a react element!</h1>
+//   </div>
+// )
 
 ReactDOM.render(
-  testElement,
+  // testElement,
+  <Auth />,
   document.getElementById('root')
 )
--- server/controllers/htmlRouter.js
+++ server/controllers/htmlRouter.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const router = express.Router()
-const adminOnly = require('../middleware/checkAuth').adminOnly
-const anyUserOnly = require('../middleware/checkAuth').anyUserOnly
+// const express = require('express')
+// const router = express.Router()
+// const adminOnly = require('../middleware/checkAuth').adminOnly
+// const anyUserOnly = require('../middleware/checkAuth').anyUserOnly
 
-router.get('/', (req, res) => {
-  console.log('User:', req.user)
-  res.render('home', {msg: 'this is a test', user: req.user})
-})
+// router.get('/', (req, res) => {
+//   console.log('User:', req.user)
+//   res.render('home', {msg: 'this is a test', user: req.user})
+// })
 
-router.get('/login', (req, res) => {
-  console.log('User:', req.user)
-  res.render('login', {msg: 'login page', user: req.user})
-})
+// router.get('/login', (req, res) => {
+//   console.log('User:', req.user)
+//   res.render('login', {msg: 'login page', user: req.user})
+// })
 
-router.get('/register', (req, res) => {
-  console.log('User:', req.user)
-  res.render('register', {msg: 'register page', user: req.user})
-})
+// router.get('/register', (req, res) => {
+//   console.log('User:', req.user)
+//   res.render('register', {msg: 'register page', user: req.user})
+// })
 
-// AUTHENTICATED ROUTES
-router.get('/user-only', anyUserOnly(), (req, res) => {
-  res.render('authPages/user-only', {user: req.user})
-})
+// // AUTHENTICATED ROUTES
+// router.get('/user-only', anyUserOnly(), (req, res) => {
+//   res.render('authPages/user-only', {user: req.user})
+// })
 
-router.get('/admin-only', adminOnly(), (req, res) => {
-  res.render('authPages/admin-only', {user: req.user})
-})
+// router.get('/admin-only', adminOnly(), (req, res) => {
+//   res.render('authPages/admin-only', {user: req.user})
+// })
 
-module.exports = router
+// module.exports = router
