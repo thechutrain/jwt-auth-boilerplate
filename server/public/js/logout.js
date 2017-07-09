ab5bf0fbfa98eaa9f9c819f3c69f5bd38b3aa785
thechutrain <chu.chuyalan@gmail.com>
2017-07-09 10:17:40 -0400

    cleaning up code

--- server/public/js/logout.js
+++ server/public/js/logout.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
-  $('a#logout-link').on('click', function(event) {
-    event.preventDefault()
-    console.log(localStorage.getItem('token'))
-    var proceed = confirm('signing you out ...')
-    if (proceed) {
-      localStorage.removeItem('token')
-      console.log(localStorage.getItem('token'))
-      debugger
-      window.location.replace(window.location.href)
-    }
-  })
-})
+// $(document).ready(function(){
+//   $('a#logout-link').on('click', function(event) {
+//     event.preventDefault()
+//     console.log(localStorage.getItem('token'))
+//     var proceed = confirm('signing you out ...')
+//     if (proceed) {
+//       localStorage.removeItem('token')
+//       console.log(localStorage.getItem('token'))
+//       debugger
+//       window.location.replace(window.location.href)
+//     }
+//   })
+// })
--- server/public/js/register.js
+++ server/public/js/register.js
@@ -1,28 +1,28 @@
-$(document).ready(function() {
-  // console.info('page was loaded')
-  $('button#register-btn').on('click', function(event) {
-    event.preventDefault()
-    // 1. Get the form data
-    var formData = {}
-    $('form > div.form-group').each(function(i, element){
-      var key = $(element).find('label').attr('for').trim()
-      var value = $(element).find('input').val().trim()
-      formData[key] = value
-    })
-    // 2. make post request
-    axios.post('/auth/register', {
-      data: formData
-    }).then(function(response) {
-      if (response.data.error) {
-        console.warn('There was an error in your sign in')
-        console.dir(response.data)
-        // console.warn(response.data.errorsArray.join(' // '))
-      } else {
-        // redirect user to login page
-        alert("Successful registration! Press okay to continue!")
-        window.location.replace('/login')
-      }
-    })
-  }) // ends register-btn event listener
-
-})
+// $(document).ready(function() {
+//   // console.info('page was loaded')
+//   $('button#register-btn').on('click', function(event) {
+//     event.preventDefault()
+//     // 1. Get the form data
+//     var formData = {}
+//     $('form > div.form-group').each(function(i, element){
+//       var key = $(element).find('label').attr('for').trim()
+//       var value = $(element).find('input').val().trim()
+//       formData[key] = value
+//     })
+//     // 2. make post request
+//     axios.post('/auth/register', {
+//       data: formData
+//     }).then(function(response) {
+//       if (response.data.error) {
+//         console.warn('There was an error in your sign in')
+//         console.dir(response.data)
+//         // console.warn(response.data.errorsArray.join(' // '))
+//       } else {
+//         // redirect user to login page
+//         alert("Successful registration! Press okay to continue!")
+//         window.location.replace('/login')
+//       }
+//     })
+//   }) // ends register-btn event listener
+//
+// })
